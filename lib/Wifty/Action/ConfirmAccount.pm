72653728486cbb96520571f5d4447f92ed31cf9a
Jesse Vincent <jesse@bestpractical.com>
2005-12-25 01:35:13 +0000

    First draft. we _clearly_ need to refactor out the email validator, if nothing else.

---
+++ lib/Wifty/Action/ConfirmAccount.pm
@@ -0,0 +1,62 @@
+use warnings;
+use strict;
+
+=head1 NAME
+
+Wifty::Action::ConfirmEmail - Confirm a user's email address
+
+=head1 DESCRIPTION
+
+This is the link in a user's email to confirm that their email
+email is really theirs.  It is not really meant to be rendered on any
+web page, but is used by the confirmation notification.
+
+Note that the use of C<insecure_url_auth_token> here is insecure and wrong!
+(XXX TODO FIXME) If an attacker knew the token calculation algorithm (including
+the non-random salt), they could easily do email confirmation without needed
+to actually have access to the email account, since the algorithm only depends on
+the email address, requested password, and non-random salt.
+
+=cut
+
+package Wifty::Action::ConfirmEmail;
+use base qw/Wifty::Action Jifty::Action/;
+
+use Wifty::Model::User;
+
+
+=head2 actions
+
+A null sub, because the superclass wants to make sure we fill in actions
+
+=cut
+
+sub actions {}
+
+=head2 take_action
+
+Set their confirmed status.
+
+=cut
+
+sub take_action {
+    my $self = shift;
+    my $u = Wifty::Model::User->new(current_user => Wifty::CurrentUser->superuser);
+    $u->load_by_cols( email => Jifty->web->current_user->user_object->email );
+
+    if ($u->email_confirmed) {
+        $self->result->error(email => "You have already confirmed your account.");
+        $self->result->success(1);  # but the action is still a success
+    }
+
+    $u->set_email_confirmed('1');
+
+    # Set up our login message
+    $self->result->message( "Welcome to Wifty, " . $u->name . ". Your email address has now been confirmed." );
+
+    # Actually do the login thing.
+    Jifty->web->current_user(Wifty::CurrentUser->new(id => $u->id));
+    return 1;
+}
+
+1;
---
+++ lib/Wifty/Action/RecoverPassword.pm
@@ -0,0 +1,7 @@
+
+use warnings;
+use strict;
+
+package Wifty::Action::RecoverPassword;
+
+1;
