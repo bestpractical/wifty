2fe256e2665e2a7db15e6937cb84e4440898ba5d
Ruslan Zakirov <ruz@bestpractical.com>
2008-11-30 00:51:15 +0000

    switch from deprecated Login plugin to User + Auth::Password plugins

--- lib/Wifty/Model/User.pm
+++ lib/Wifty/Model/User.pm
@@ -1,4 +1,13 @@
 package Wifty::Model::User;
-use base qw/Jifty::Plugin::Login::Model::User/;
+
+use Jifty::DBI::Schema;
+use Wifty::Record schema {
+    # column definitions
+};
+
+# import columns: name, email and email_confirmed
+use Jifty::Plugin::User::Mixin::Model::User;
+# import columns: password, auth_token
+use Jifty::Plugin::Authentication::Password::Mixin::Model::User;
 
 1;
